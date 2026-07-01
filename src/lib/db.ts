a7764adaf61481c5c833f7e2e8da89fb0504984c
abdalahe <abdalaheh51@gmail.com>
2026-07-01 17:58:02 +0300

    fix4

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,4 +1,5 @@
-import 'dotenv/config'
+import dotenv from 'dotenv'
+dotenv.config({ path: '.env' })
 import { PrismaClient } from '@prisma/client'
 
 const globalForPrisma = globalThis as unknown as {
